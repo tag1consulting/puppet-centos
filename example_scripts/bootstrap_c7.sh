d77bd23cfa22b35323f0cea80b8eb1db46f5a0cf
Jeff Sheltren <jeff@tag1consulting.com>
2015-07-03 07:13:32 -0700

    Add Vagrantfile specific to EL7 (using CentOS official C7 Vagrant Box) Update EL7 bootstrap.sh example to correct rubygem install and be more verbose when installing packages.

--- example_scripts/bootstrap_c7.sh
+++ example_scripts/bootstrap_c7.sh
@@ -6,30 +6,23 @@
 # This script assumes a CentOS 7.x host.
 
 # Need git for r10k
-rpm -q --quiet git || yum -y install git
+rpm -q --quiet git || { echo "git package not found, installing with yum"; yum -y install git; }
 
 # Need rubygems for r10k.
-rpm -q --quiet rubygems || yum -y install rubygems
+rpm -q --quiet rubygems || { echo "rubygems package not found, installing with yum"; yum -y install rubygems; }
 
 # Install Puppet repos. Note: EPEL is installed by default on Rackspace CentOS images.
-rpm -q --quiet puppetlabs-release || rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+rpm -q --quiet puppetlabs-release || { echo "puppetlabs-release package not found, installing with rpm"; rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm; }
 
 # It helps to have puppet installed...
-rpm -q --quiet puppet || yum -y install puppet
+rpm -q --quiet puppet || { echo "puppet package not found, installing with yum"; yum -y install puppet; }
 
 # We need rubygem-deep-merge for the 'deeper' merge setting used in Hiera.
 # This is provided in the puppetlabs-deps repo (installed above).
-rpm -q --quiet rubygem-deep-merge || yum -y install rubygem-deep-merge
+rpm -q --quiet rubygem-deep_merge || { echo "rubygem-deep_merge package not found, installing with yum"; yum -y install rubygem-deep_merge; }
 
 # Install r10k to handle Puppet module installation.
-[[ "$(gem query -i -n r10k)" == "true" ]] || gem install --no-rdoc --no-ri r10k
+[[ "$(gem query -i -n r10k)" == "true" ]] || { echo "r10k gem not found, installing with gem"; gem install --no-rdoc --no-ri r10k; }
 
 # Run r10k to pull in external modules.
-cd /root/puppet && r10k -v info puppetfile install
-
-# The following is only required on Rackspace.
-# Rackspace Vagrant plugin seems to run puppet with /tmp module paths no matter what we configure.
-# And the number in the directory /tmp/vagrant-puppet-# seems to change occasionally -- and is not well
-# documented in Vagrant or the Rackspace plugin.
-# This is a hacky workaround just to get things working without too much effort.
-#rm -fr /tmp/vagrant-puppet-2/modules-0 && ln -s /vagrant/modules /tmp/vagrant-puppet-2/modules-0
+cd /vagrant && /usr/local/bin/r10k -v info puppetfile install
