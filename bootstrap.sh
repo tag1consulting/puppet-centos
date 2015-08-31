e3da53eb22706f4b2627dac0327fdd7e215a0806
Greg Lund-Chaix <greg@tag1consulting.com>
2015-08-31 10:21:16 -0700

    Make bootstrap.sh aware of the OS major version and act accordingly

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,34 +3,47 @@
 ##
 # bootstrap.sh: preps our environment for puppet.
 # Installs r10k, puppet, hiera, and their dependencies.
-# This script assumes a CentOS 6.x host.
+
+# Detect major version
+OSVER=`rpm -q --queryformat "%{VERSION}" centos-release`
+echo "Bootstrapping CentOS $OSVER"
 
 # Need git for r10k
-rpm -q --quiet git || yum -y install git
+rpm -q --quiet git || { echo "git package not found, installing with yum"; yum -y install git; }
 
 # Need rubygems for r10k.
-rpm -q --quiet rubygems || yum -y install rubygems
+rpm -q --quiet rubygems || { echo "rubygems package not found, installing with yum"; yum -y install rubygems; }
 
 # Install Puppet repos. Note: EPEL is installed by default on Rackspace CentOS images.
-rpm -q --quiet puppetlabs-release || rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+rpm -q --quiet puppetlabs-release || { echo "puppetlabs-release package not found, installing with rpm"; rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-el-${OSVER}.noarch.rpm; }
 
 # It helps to have puppet installed...
-rpm -q --quiet puppet || yum -y install puppet
-
-# We need rubygem-deep-merge for the 'deeper' merge setting used in Hiera.
-# This is provided in the puppetlabs-deps repo. We assume this repo is already
-# configured in the base box.
-rpm -q --quiet rubygem-deep-merge || yum -y install rubygem-deep-merge
-
-# We need ruby-devel in order to install the system_timer gem.
-rpm -q --quiet ruby-devel || yum -y install ruby-devel
+rpm -q --quiet puppet || { echo "puppet package not found, installing with yum"; yum -y install puppet; }
 
 # Install r10k to handle Puppet module installation.
-# EL6 has Ruby 1.8, so we need to pull in the latest 1.x version of r10k
-[[ "$(gem query -i -n r10k)" == "true" ]] || gem install --no-rdoc --no-ri r10k -v 1.5.1
+if [ "$OSVER" -eq "6" ]; then
+  # We need rubygem-deep-merge for the 'deeper' merge setting used in Hiera.
+  # This is provided in the puppetlabs-deps repo. We assume this repo is already
+  # configured in the base box.
+  rpm -q --quiet rubygem-deep-merge || { echo "rubygem-deep-merge package not found, installing with yum"; yum -y install rubygem-deep-merge; }
 
-# system_timer ruby gem is needed to avoid warnings on the CentOS 6 version of Ruby (1.8)
-[[ "$(gem query -i -n system_timer)" == "true" ]] || gem install --no-rdoc --no-ri system_timer
+  # EL6 has Ruby 1.8, so we need to pull in the latest 1.x version of r10k
+  [[ "$(gem query -i -n r10k)" == "true" ]] || { echo "r10k gem not found, installing with gem"; gem install --no-rdoc --no-ri r10k -v 1.5.1; }
+
+  # We need ruby-devel in order to install the system_timer gem.
+  rpm -q --quiet ruby-devel || { echo "ruby-devel package not found, installing with yum"; yum -y install ruby-devel; }
+
+  # system_timer ruby gem is needed to avoid warnings on the CentOS 6 version of Ruby (1.8)
+  [[ "$(gem query -i -n system_timer)" == "true" ]] || gem install --no-rdoc --no-ri system_timer
+else
+  # We need rubygem-deep-merge for the 'deeper' merge setting used in Hiera.
+  # This is provided in the puppetlabs-deps repo. We assume this repo is already
+  # configured in the base box.
+  rpm -q --quiet rubygem-deep_merge || { echo "rubygem-deep_merge package not found, installing with yum"; yum -y install rubygem-deep_merge; }
+
+  # Install r10k to handle Puppet module installation.
+  [[ "$(gem query -i -n r10k)" == "true" ]] || { echo "r10k gem not found, installing with gem"; gem install --no-rdoc --no-ri r10k; }
+fi
 
 # Run r10k to pull in external modules.
 cd /vagrant && r10k -v info puppetfile install
