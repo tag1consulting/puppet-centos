5e269d8e43671d669da852efafad82105055bfe9
Greg Lund-Chaix <greg@tag1consulting.com>
2015-08-31 14:27:09 -0700

    No chown

--- scripts/post-puppet.sh
+++ scripts/post-puppet.sh
@@ -4,4 +4,4 @@
 /vagrant/scripts/drush8.sh 2>&1 > /dev/null
 
 # Ensure Drupal can write settings and files
-chown -R apache /var/www/vagrant-multi1.tag1consulting.com/sites/default
+#chown -R apache /var/www/vagrant-multi1.tag1consulting.com/sites/default
